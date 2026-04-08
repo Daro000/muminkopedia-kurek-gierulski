78422808a8fc8482586b7454af8c4b34e6dd1fa5
Daro <twojemail@example.com>
2026-04-08 18:25:06 +0200

    Add functionality to app

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,23 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import dotenv from "dotenv";
+import characterRoutes from "./routes/characterRoutes.js";
+import artifactRoutes from "./routes/artifactRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
-app.use(express.json()); // Parsowanie JSON w requestach
+app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/", (req, res) => {
     res.json({ message: "API Express + TypeScript działa!" });
 });
+
+app.use("/api/characters", characterRoutes);
+app.use("/api/artifacts", artifactRoutes);
 
 export default app;
